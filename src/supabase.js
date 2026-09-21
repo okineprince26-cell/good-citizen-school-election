de46f1145c0767a60183cbd46ad1d1527be498ec
okineprince26-cell <okineprince26@gmail.com>
2026-09-21 03:30:16 +0000

    Create supabase.js

---
+++ src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
